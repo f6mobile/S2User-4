a696e590a4ab10c4b29666cd1bb774177891f39f
user <f6mobile.vspc@gmail.com>
2022-06-16 18:17:13 +0300

    s

--- app/src/main/java/com/denis/myapplication/homeActivity.java
+++ app/src/main/java/com/denis/myapplication/homeActivity.java
@@ -1,36 +1,39 @@
 package com.denis.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.denis.myapplication.databinding.ActivityHomeBinding;
 import com.denis.myapplication.databinding.ActivityMainBinding;
 
-public class MainActivity extends Activity {
+public class homeActivity extends Activity {
+
+    Button button;
 
     private TextView mTextView;
-    private ActivityMainBinding binding;
-    Button button;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        button = ( Button) findViewById(R.id.button);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        button = (Button) findViewById(R.id.button);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(intent);
 
             }
         });
-
-
-
     }
 }
